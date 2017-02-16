6c5f067337f4d212cbebe433948394e63318eae2
cfgs <cfgs@pc-info-host>
2017-02-16 16:33:24 +0100

    Branca noves funcionalitats

--- vehicles/src/vehicles/Vehicle.java
+++ vehicles/src/vehicles/Vehicle.java
@@ -3,15 +3,20 @@ package vehicles;
 public class Vehicle {
 	
 	public static final int velocitat_maxima = 120;
+	public static final int marxes = 5;
 	
 	private int cilindrada;
 	private int portes;
 	private int velocidad_actual;
+	private int marxa_actual;
+	
+	
 	public Vehicle(int cilindrada, int portes) {
 		super();
 		this.cilindrada = cilindrada;
 		this.portes = portes;
 		this.velocidad_actual = 0;
+		this.marxa_actual = 0;
 	}
 	
 	
@@ -33,7 +38,14 @@ public class Vehicle {
 	public void setVelocidad_actual(int velocidad_actual) {
 		this.velocidad_actual = velocidad_actual;
 	}
-	
+	public int getMarxa_actual() {
+		return marxa_actual;
+	}
+	public void setMarxa_actual(int marxa_actual) {
+		this.marxa_actual = marxa_actual;
+	}
+
+
 	public void Accelerar() {
 		if ((this.velocidad_actual + 10) <= velocitat_maxima) {
 			this.velocidad_actual = this.velocidad_actual + 10;
@@ -43,6 +55,18 @@ public class Vehicle {
 	public void Decelerar() {
 		if ((this.velocidad_actual - 10) >= 0) {
 			this.velocidad_actual = this.velocidad_actual - 10;
+		}
+	}
+	
+	public void pujaMarxa() {
+		if ((this.marxa_actual + 1) <= marxes) {
+			this.marxa_actual++;
+		}
+	}
+	
+	public void quitaMarxa() {
+		if ((this.marxa_actual - 1) >= 0) {
+			this.marxa_actual--;
 		}
 	}
 	
